a7eaabde78291950f3765beeea7eac1029493518
GuoHaoLan <coopticks@hotmail.com>
2019-08-06 17:36:26 +0000

    Update Composer

---
+++ t.php
@@ -0,0 +1,7 @@
+<?php
+require 'real_autoload.php';
+
+var_dump(\XSYD\Password\SecureRandom::GenSalt());
+
+
+?>
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'XSYD\\User\\' => array($vendorDir . '/xsyd/user'),
+    'XSYD\\' => array($vendorDir . '/xsyd/tools'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
     'League\\OAuth2\\Server\\' => array($vendorDir . '/league/oauth2-server/src'),
